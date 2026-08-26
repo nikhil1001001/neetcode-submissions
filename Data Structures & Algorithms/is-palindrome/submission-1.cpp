b1afa005b1e0af640a48b6a551d6cb6b5f4a660c
Nikhil Yadav <122379503+nikhil1001001@users.noreply.github.com>
2026-08-26 18:26:01 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool notValid(char a){
+        if(a<='Z' && a>='A')return false;
+        if(a<='z' && a>='a')return false;
+        if(a<='9' && a>='0')return false;
+        return true;
+    } 
+
+    bool isPalindrome(string s) {
+        int n = s.length();
+        int i =0, j = n-1;
+
+        while(i<j){
+            if(s[i]==' ' || notValid(s[i])){
+                i++;
+                continue;
+            }
+            if(s[j]==' ' || notValid(s[j])){
+                j--;
+                continue;
+            }
+            if(tolower(s[i])!=tolower(s[j])){
+                // int x=0,y=0;
+                // if((s[i]<='9' && s[i]>='0') || (s[j]<='9' && s[j]>='0'))return false;
+                // if(s[i]<='Z' && s[i]>='A') x= s[i]-'A';
+                // if(s[i]<='z' && s[i]>='a') x= s[i]-'a';
+                // if(s[j]<='Z' && s[j]>='A') y= s[j]-'A';
+                // if(s[j]<='z' && s[j]>='a') y= s[j]-'a';
+                
+                // if(x==y) {
+                //     i++;
+                //     j--;
+                //     continue;
+                // }
+
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+};
